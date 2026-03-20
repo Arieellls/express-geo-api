e8379613690360f477d6b156305806eb3fa16ad4
Arielito Manorina <arielitorobles1525@gmail.com>
2026-03-20 17:41:03 +0800

    update default server port to 4000

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import {
 import { registerRoutes } from "./routes/index.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
